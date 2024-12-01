4eec482454da86a277fd9b3f016e5b93d9ea7437
Raushan Kumar <raushan0785@gmail.com>
2024-12-01 17:21:57 +0530

    controller setup

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,4 @@
 module.exports.home=function(req,res){
-    //res.end('<h1>exprerss</h1> <a href="C:\Users\Lenovo\Pictures\Screenshots\Screenshot 2024-07-27 222425.png">');
-    return res.render('home',{
-        title:"home"
-    });
+    res.end('<h1>express is fire up in codeial');
+
 }
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
 const express=require('express');
 const router=express.Router();
-console.log('router loaded');
+const homecontroller=require('../controllers/home_controller.js');
+router.get('/',homecontroller.home);
+module.exports=router;
