15c3be3936ef1317f59efcd775aa9dedfbd02864
Raushan Kumar <raushan0785@gmail.com>
2024-12-02 15:16:08 +0530

    setup user schema using monodb

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const app=express();
 const port=8000;
+//use express router
 app.use('/',require('./routes/index'));
+app.use('/',require('./routes/users'));
+//setup view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log('error',err);
--- models/user.js
+++ models/user.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose');
+const userschema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
     },
-    password: {
-        type: String,
-        required: true
+    password:{
+        type:String,
+        required:true
+
     },
-    name: {
-        type: String,
-        required: true
-    }
+    name:{
+        type:String,
+        required:true
+    },
+  
+
+
 }, {
-    timestamps: true
-});
-
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+    timestamp:true
+   });
+   const User=mongoose.model('User',userschema);
+   module.exports=User;
