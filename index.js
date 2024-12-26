8f16a618de2272fda98f6bc7cda06c8a4d351f53
Raushan Kumar <raushan0785@gmail.com>
2024-12-26 21:37:45 +0530

    setting up mongo store for sessio cookies

--- index.js
+++ index.js
@@ -6,23 +6,32 @@ const db=require('./config/mongoose');
 //used for session cookie
 const session=require('express-session');
 const Passport=require('passport');
+
 const PassportLocal=require('./config/passport-local-strategy');
 const passport = require('passport');
+const mongostore=require('connect-mongo');
 app.use(express.urlencoded());
 app.use(cookieparser());
 
 //setup view engine
 app.set('view engine','ejs');
 app.set('views','./views');
-app.use(session({
-    name:'codeial',
-    secret:'blahsomething',
-    saveUninitialized:false,
-    resave:false,
-    cookie:{
-        maxage:(1000*60*100)
-    }
-}));
+
+app.use(
+    session({
+        name: 'codeial',
+        secret: 'blahsomething', // Replace with a strong secret key
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 1000 * 60 * 100, // Session expiration time
+        },
+        store: mongostore.create({
+            mongoUrl: 'mongodb://localhost:27017/codeial', // MongoDB connection string
+            autoRemove: 'disabled',
+        }),
+    })
+);
 app.use(Passport.initialize());
 app.use(Passport.session());
 app.use(passport.setAuthenticateduser);
